84aeb024acc98c186d486778677815fd11aff813
Henry Müssemann <hm@maltego.com>
2024-05-07 13:01:25 +0200

    SNH-2433: Trying to fix test

--- test/test_proxy.py
+++ test/test_proxy.py
@@ -24,6 +24,10 @@ class TestProxy(unittest.TestCase):
             chrome_options = webdriver.ChromeOptions()
             # https://stackoverflow.com/questions/58453327/python-selenium-chrome-extensions-with-headless-not-working
             chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+
             proxy_helper = SeleniumAuthenticatedProxy(
                 proxy_url=PROXY_URL
             )
